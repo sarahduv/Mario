3fa8e5e44ca997d72a66e190777ef60cb9282954
sarahduv <sarah.duvdevani@gmail.com>
2020-02-10 12:24:23 -0800

    background scroll on key down working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,67 +30,70 @@
         {
             this.components = new System.ComponentModel.Container();
             this.container = new System.Windows.Forms.Panel();
-            this.gravityTimer = new System.Windows.Forms.Timer(this.components);
             this.mario = new System.Windows.Forms.PictureBox();
-            this.background2 = new System.Windows.Forms.PictureBox();
             this.background1 = new System.Windows.Forms.PictureBox();
+            this.movementTimer = new System.Windows.Forms.Timer(this.components);
+            this.background2 = new System.Windows.Forms.PictureBox();
             this.container.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mario)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.background2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.background1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background2)).BeginInit();
             this.SuspendLayout();
             // 
             // container
             // 
             this.container.Controls.Add(this.mario);
-            this.container.Controls.Add(this.background2);
             this.container.Controls.Add(this.background1);
+            this.container.Controls.Add(this.background2);
             this.container.Location = new System.Drawing.Point(-5, -4);
             this.container.Name = "container";
-            this.container.Size = new System.Drawing.Size(1095, 606);
+            this.container.Size = new System.Drawing.Size(1194, 656);
             this.container.TabIndex = 0;
-            // 
-            // gravityTimer
-            // 
-            this.gravityTimer.Tick += new System.EventHandler(this.gravityTimer_Tick);
             // 
             // mario
             // 
             this.mario.BackColor = System.Drawing.Color.White;
             this.mario.Image = global::Mario.Properties.Resources.mario;
-            this.mario.Location = new System.Drawing.Point(378, 522);
+            this.mario.Location = new System.Drawing.Point(370, 570);
             this.mario.Name = "mario";
             this.mario.Size = new System.Drawing.Size(30, 47);
             this.mario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.mario.TabIndex = 3;
             this.mario.TabStop = false;
             // 
-            // background2
-            // 
-            this.background2.BackgroundImage = global::Mario.Properties.Resources.background;
-            this.background2.Location = new System.Drawing.Point(1044, -49);
-            this.background2.Name = "background2";
-            this.background2.Size = new System.Drawing.Size(1153, 653);
-            this.background2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.background2.TabIndex = 2;
-            this.background2.TabStop = false;
-            // 
             // background1
             // 
+            this.background1.BackColor = System.Drawing.Color.White;
             this.background1.BackgroundImage = global::Mario.Properties.Resources.background;
-            this.background1.Location = new System.Drawing.Point(-58, -49);
+            this.background1.Location = new System.Drawing.Point(-27, 0);
             this.background1.Name = "background1";
-            this.background1.Size = new System.Drawing.Size(1153, 653);
+            this.background1.Size = new System.Drawing.Size(1290, 653);
             this.background1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.background1.TabIndex = 1;
             this.background1.TabStop = false;
+            // 
+            // movementTimer
+            // 
+            this.movementTimer.Enabled = true;
+            this.movementTimer.Tick += new System.EventHandler(this.movementTimer_Tick);
+            // 
+            // background2
+            // 
+            this.background2.BackColor = System.Drawing.Color.White;
+            this.background2.BackgroundImage = global::Mario.Properties.Resources.background;
+            this.background2.Location = new System.Drawing.Point(450, 0);
+            this.background2.Name = "background2";
+            this.background2.Size = new System.Drawing.Size(1290, 653);
+            this.background2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background2.TabIndex = 2;
+            this.background2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(1088, 678);
+            this.ClientSize = new System.Drawing.Size(1180, 721);
             this.Controls.Add(this.container);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -98,8 +101,8 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.stop);
             this.container.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.mario)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.background2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.background1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -107,10 +110,10 @@
         #endregion
 
         private System.Windows.Forms.Panel container;
-        private System.Windows.Forms.PictureBox background2;
         private System.Windows.Forms.PictureBox background1;
-        private System.Windows.Forms.Timer gravityTimer;
+        private System.Windows.Forms.Timer movementTimer;
         private System.Windows.Forms.PictureBox mario;
+        private System.Windows.Forms.PictureBox background2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 
+
 namespace Mario
 {
     public partial class Form1 : Form
@@ -18,22 +19,40 @@ namespace Mario
 
         {
             InitializeComponent();
+            background1.Left = -58;
+            background2.Left = 950;
         }
 
-        private void gravityTimer_Tick(object sender, EventArgs e)
+        private void movementTimer_Tick(object sender, EventArgs e)
         {
-            background1.Left -= backgroundSpeed;
-            background2.Left -= backgroundSpeed;
 
-            if (background1.Left < 0)
+            background1.Left += backgroundSpeed;
+            background2.Left += backgroundSpeed;
+
+            if (background1.Left < -900)
             {
                 background1.Left = 1050;
             }
 
+            if (background2.Left < -1000)
+            {
+                background2.Left = 950;
+            }
+
+
+
+            /*background1.Location = new Point(background1.Location.X + backgroundSpeed, background1.Location.Y);
+            background2.Location = new Point(background2.Location.X + backgroundSpeed, background2.Location.Y);
+
+            if (background1.Left < 0)
+            {
+                background1.Location = new Point(background1.Location.X + 1050, background1.Location.Y);
+            }
+
             if (background2.Left < 0)
             {
-                background2.Left = 1050;
-            }
+                background2.Location = new Point(background2.Location.X + 1050, background2.Location.Y);
+            }*/
         }
 
         private void move(object sender, KeyEventArgs e)
@@ -41,19 +60,23 @@ namespace Mario
             Debug.WriteLine("key: " + e.KeyCode);
             if (e.KeyCode == Keys.Left)
             {
-                backgroundSpeed += 5;
+                backgroundSpeed += 1;
             }
 
             if (e.KeyCode == Keys.Right)
             {
-                backgroundSpeed -= 5;
+                backgroundSpeed -= 1;
             }
         }
 
         private void stop(object sender, KeyEventArgs e)
         {
             Debug.WriteLine("key: " + e.KeyCode);
-            if (e.KeyCode == Keys.Down)
+            if (e.KeyCode == Keys.Left)
+            {
+                backgroundSpeed = 0;
+            }
+            if (e.KeyCode == Keys.Right)
             {
                 backgroundSpeed = 0;
             }
